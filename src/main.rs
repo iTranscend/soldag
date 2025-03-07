7fbf5ab5325e0e7c1b8ea3685e93fad870b8a27f
iTranscend <llordbanks500@gmail.com>
2025-03-07 06:08:51 +0100

    feat: initialize services on separate threads

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
 use clap::Parser;
 use log::{error, info};
 
+use domain::utils::PostgresUrl;
+
+mod api;
 mod cli;
+mod domain;
+mod indexer;
+mod logger;
 
 async fn init() -> anyhow::Result<()> {
     let args = cli::Args::parse();
 
+    dotenv::dotenv().ok();
+
+    // Collect db url from env variables
+    let database_url = PostgresUrl::from_env()?.to_string();
+
+    let storage = domain::storage::Storage::new(&database_url).await?;
+
+    let indexer = indexer::Indexer::new(args.rpc_url, args.rpc_api_key.as_deref(), storage).await?;
+
+    let mut indexer_handle = tokio::spawn(indexer.clone().start(args.update_interval));
+    let mut api_handle = tokio::spawn(api::start(args.api_listen));
+
+    // retry 3 times
+    for _ in 1..=3 {
+        tokio::select! {
+            res = &mut indexer_handle => {
+                if let Ok(Err(e)) = res {
+                    error!("Indexer service failed: {}", e)
+                }
+                indexer_handle = tokio::spawn(indexer.clone().start(args.update_interval))
+            }
+            res = &mut api_handle => {
+                if let Ok(Err(e)) = res {
+                    error!("API service failed: {}", e)
+                }
+                api_handle = tokio::spawn(api::start(args.api_listen))
+
+            }
+        }
+    }
     Ok(())
 }
 
