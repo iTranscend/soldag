0697bc202f825803257ebcdfed0238adfd29eb0a
iTranscend <llordbanks500@gmail.com>
2025-03-12 16:39:47 +0100

    feat: paginate tx api

--- src/api.rs
+++ src/api.rs
@@ -3,19 +3,20 @@ use std::{fmt::Debug, net::SocketAddr, sync::Arc};
 use axum::{
     debug_handler,
     extract::{Query, State},
-    response::{IntoResponse, Response},
     routing::get,
     Json, Router,
 };
 use chrono::NaiveDate;
-use eyre::eyre;
 use http::StatusCode;
 use log::{error, info};
 use serde::{Deserialize, Serialize};
 
-use mongodb::bson::Document;
+use solana_sdk::account::Account;
 
-use crate::domain::{models::transaction::Transaction, storage::Storage};
+use crate::{
+    domain::{models::transaction::Transaction, storage::Storage},
+    indexer::Indexer,
+};
 
 #[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
@@ -38,66 +39,88 @@ pub struct TransactionResponse {
     pub next: Option<u64>,
 }
 
-#[derive(Clone)]
-pub struct Api {
-    storage: Arc<Storage>,
+async fn fetch_transactions(
+    Query(params): Query<Paginated<TransactionQuery>>,
+    State((storage, _)): State<(Arc<Storage>, Indexer)>,
+) -> Result<Json<TransactionResponse>, (StatusCode, String)> {
+    let date = if let Some(day) = params.data.day {
+        let date = NaiveDate::parse_from_str(&day, "%d/%m/%Y")
+            .map_err(|e| (StatusCode::BAD_REQUEST, format!("Invalid date: {}", e)))?;
+        Some(date.and_hms_opt(0, 0, 0).expect("Infallible").and_utc())
+    } else {
+        None
+    };
+
+    let (data, next) = match storage
+        .get_transactions(
+            params.data.id,
+            date,
+            params.count.unwrap_or(10),
+            params.offset.unwrap_or(0),
+        )
+        .await
+    {
+        Ok(res) => res,
+        Err(e) => {
+            error!("Error fetching transactions: {}", e);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error fetching transactions".to_string(),
+            ));
+        }
+    };
+
+    let response = TransactionResponse { data, next };
+
+    Ok(Json(response))
 }
 
-impl Api {
-    pub async fn new(storage: Arc<Storage>) -> eyre::Result<Self> {
-        Ok(Self { storage })
-    }
+#[derive(Serialize, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct AccountQuery {
+    pubkey: String,
+}
 
-    // #[debug_handler]
-    async fn fetch_transactions(
-        Query(params): Query<Paginated<TransactionQuery>>,
-        State(storage): State<Arc<Storage>>,
-    ) -> Result<Json<TransactionResponse>, (StatusCode, String)> {
-        dbg!(&params);
-        let date = if let Some(day) = params.data.day {
-            let date = NaiveDate::parse_from_str(&day, "%d/%m/%Y")
-                .map_err(|e| (StatusCode::BAD_REQUEST, format!("Invalid date: {}", e)))?;
-            Some(date.and_hms_opt(0, 0, 0).expect("Infallible").and_utc())
-        } else {
-            None
-        };
+#[derive(Serialize, Debug)]
+pub struct AccountResponse {
+    pub data: Account,
+}
 
-        let (data, next) = match storage
-            .get_transactions(
-                params.data.id,
-                date,
-                params.count.unwrap_or(10),
-                params.offset.unwrap_or(0),
-            )
-            .await
-        {
-            Ok(res) => res,
-            Err(e) => {
-                error!("Error fetching transactions: {}", e);
-                return Err((
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Error fetching transactions".to_string(),
-                ));
-            }
-        };
+#[debug_handler]
+async fn fetch_account(
+    Query(params): Query<AccountQuery>,
+    State((_, indexer)): State<(Arc<Storage>, Indexer)>,
+) -> Result<Json<AccountResponse>, (StatusCode, String)> {
+    let data = match indexer.get_account(params.pubkey).await {
+        Ok(res) => res,
+        Err(e) => {
+            error!("Error fetching transactions: {}", e);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error fetching transactions".to_string(),
+            ));
+        }
+    };
 
-        let response = TransactionResponse { data, next };
-        dbg!(&response);
+    let response = AccountResponse { data };
 
-        Ok(Json(response))
-    }
+    Ok(Json(response))
+}
 
+pub async fn start(
+    api_listen: SocketAddr,
+    storage: Arc<Storage>,
+    indexer: Indexer,
+) -> eyre::Result<()> {
+    let app = Router::new()
+        .route("/transactions", get(fetch_transactions))
+        .route("/accounts", get(fetch_account))
+        .with_state((storage, indexer));
 
-    pub async fn start(self, api_listen: SocketAddr) -> eyre::Result<()> {
-        let app = Router::new()
-            .route("/transactions", get(Self::fetch_transactions))
-            .with_state(self.storage);
+    info!("Starting API server on {}", api_listen);
 
-        info!("Starting API server on {}", api_listen);
+    let listener = tokio::net::TcpListener::bind(api_listen).await?;
+    axum::serve(listener, app).await?;
 
-        let listener = tokio::net::TcpListener::bind(api_listen).await?;
-        axum::serve(listener, app).await?;
-
-        Ok(())
-    }
+    Ok(())
 }
